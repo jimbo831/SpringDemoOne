1373cd02067330757f76bbf5fcfe89b0585d26ab
jimbo831 <jimbo831@gmail.com>
2016-11-15 22:52:26 -0600

    Implemented set by value in xml for CricketCoach

--- src/main/java/com/johnbwhitejr/springdemo/SetterDemoApp.java
+++ src/main/java/com/johnbwhitejr/springdemo/SetterDemoApp.java
@@ -17,6 +17,10 @@ public class SetterDemoApp {
         CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
 
         // call methods on the bean
+        System.out.println(theCoach.getDailyWorkout());
+        System.out.println(theCoach.getDailyFortune());
+        System.out.println(theCoach.getEmailAddress());
+        System.out.println(theCoach.getTeam());
 
         // close the context
         context.close();
